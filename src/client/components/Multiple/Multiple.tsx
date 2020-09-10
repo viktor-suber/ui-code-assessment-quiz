7825d01b56a4cb7069754a033d97fd79bd54c270
Viktor Suber <viktor.suber@gmail.com>
2020-09-09 23:07:25 -0700

    remove dummy data

--- src/client/components/Multiple/Multiple.tsx
+++ src/client/components/Multiple/Multiple.tsx
@@ -19,23 +19,10 @@ export const Multiple: React.FC<Multiple> = (props) => {
 
   const { register, handleSubmit, errors } = useForm();
 
-  const tempQuestion = 'Which company did Valve cooperate with in the creation of the Vive?';
-
-  const tempAnswers = ["HTC",  "Oculus", "Google", "Razer"];
-
-  const tempCorrectAnswer = "HTC";
-
-
   useEffect(() => {
-    console.log('PROPS', props);
     setQuestion(props.question);
     setCorrectAnswer(props.correctAnswer);
     setAnswers(props.answers);
-
-    // setQuestion(tempQuestion);
-    // setCorrectAnswer(tempCorrectAnswer);
-    // setAnswers(tempAnswers);
-
   }, [props]);
 
   const onSubmit = (event: any) => {
